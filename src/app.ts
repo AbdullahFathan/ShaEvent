8b90458573d78135248edf1a90b996d679079c5a
AbdullahFathan <abdullahfathan013@gmail.com>
2026-01-28 22:05:55 +0700

    Feat: add packages

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/check", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Server is running" });
 });
 
 app.listen(port, () => {
